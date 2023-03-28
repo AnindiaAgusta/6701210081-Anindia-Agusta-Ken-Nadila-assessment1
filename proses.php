c1a9ec040147fcffc6f31eda04f9d43c69785f2e
AnindiaAgusta <126650401+AnindiaAgusta@users.noreply.github.com>
2023-03-28 10:31:19 +0700

    v1.3

--- proses.php
+++ proses.php
@@ -1,1 +1,21 @@
+<?php
+		$angka = $_POST["angka"];
+		$angka2 = $_POST["angka2"];
+		$operator = $_POST["operator"];
 
+		if ($operator == "+") {
+			$hasil = $angka + $angka2;
+			echo "<p>Hasil dari $angka + $angka2 adalah $hasil</p>";
+		} elseif ($operator == "-") {
+			$hasil = $angka - $angka2;
+			echo "<p>Hasil dari $angka - $angka2 adalah $hasil</p>";
+		} elseif ($operator == "*") {
+			$hasil = $angka * $angka2;
+			echo "<p>Hasil dari $angka x $angka2 adalah $hasil</p>";
+		} elseif ($operator == "/") {
+			$hasil = $angka / $angka2;
+			echo "<p>Hasil dari $angka / $angka2 adalah $hasil</p>";
+		} else {
+			echo "<p>Operator tidak dikenali</p>";
+		}
+?>
